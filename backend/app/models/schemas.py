ac4b36f74fbc1073eca92db80002944348d8802c
Addisu <addisuhaile87@gmail.com>
2025-03-03 15:51:46 +0300

    Add EmailData schema

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -112,3 +112,8 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
+
+
+class EmailData(SQLModel):
+    html_content: str
+    subject: str
